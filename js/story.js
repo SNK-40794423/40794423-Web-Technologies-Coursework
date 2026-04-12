7c09c70952c48aabf9ed5d4d222f76afb4128546
SNK-40794423 <40794423@live.napier.ac.uk>
2026-04-12 17:13:20 +0630

    story js update

--- js/story.js
+++ js/story.js
@@ -167,7 +167,7 @@ function createCharacter(key, slotIndex) {
 
 // UPDATE DIALOGUE
 
-const faeeChapters = [4, 5, 6, 7, 8, 9, 10];
+const faeeChapters = [ 5, 6, 7, 8, 9, 10];
 
 function updateDialogue() {
 
